28e89f4cce7d2662d145ebeae46f5ee22680d573
gaetanbouchard <gaetan.bouchard-gruffaz@etu.univ-smb.fr>
2023-10-17 08:59:52 +0200

    removed useless images from the file

--- src/humanDetector1000.py
+++ src/humanDetector1000.py
@@ -3,8 +3,7 @@ from ultralytics import YOLO
 
 images = load_images("data/images_rgb")
 
-if __name__ == "__man__":
-    
-    model = YOLO("model.pt")
+if __name__ == "__main__":
+    model = YOLO("yolov8n.pt")
     results = model.predict(source=images)
     print(results)
